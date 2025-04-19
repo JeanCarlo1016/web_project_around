ef69602f02ae549bc7ddee16f40095ed1a1dcd2c
JeanCarlo <jeanca101699@gmail.com>
2025-04-19 15:52:14 -0500

    Feature/javascript done

--- scripts/index.js
+++ scripts/index.js
@@ -4,28 +4,29 @@ let buttonClose = document.querySelector(".popup__button-cancel");
 let formElement = document.querySelector(".popup__container");
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about");
-let nameInput = document.querySelector(".form__name");
-let aboutInput = document.querySelector(".form__about");
+let nameInput = document.querySelector(".popup__name");
+let aboutInput = document.querySelector(".popup__about");
 
-console.log(profileName);
-console.log(profileAbout);
-console.log(nameInput);
-console.log(aboutInput);
 
 function openPopup() {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
-  popup.classList.toggle("popup_opened");
+  popup.style = "display:block";
+}
+
+function closePopup() {
+  popup.style = "display:none";
 }
 
 buttonEdit.addEventListener("click", openPopup);
-buttonClose.addEventListener("click", openPopup);
+buttonClose.addEventListener("click", closePopup);
 
 function saveChange(e) {
   e.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
   openPopup();
+  closePopup();
 }
 
 formElement.addEventListener("submit", saveChange);
